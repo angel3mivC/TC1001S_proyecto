76f1e49c880c45d5f210a7e8801102aa4bf09eed
Alien300owo <PaoloMar_@hotmail.com>
2024-09-12 15:19:44 -0600

    La funcion de un publicador y suscriptor unidos

---
+++ publisher_&_subscriber.js
@@ -0,0 +1,34 @@
+const mqtt = require('mqtt')
+const protocol = 'mqtt'
+const host = '44.204.31.211'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `${protocol}://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'Dragon',
+    password: 'Contrasena',
+    reconnectPeriod: 1000,
+  })
+  const topic = '/nodejs/mqtt'
+
+  client.on('connect', () => {
+    console.log('Connected')
+  
+    client.subscribe([topic], () => {
+      console.log(`Subscribe to topic '${topic}'`)
+      client.publish(topic, 'nodejs mqtt Prueba para publicador y suscriptor', { qos: 0, retain: false }, (error) => {
+        if (error) {
+          console.error(error)
+        }
+      })
+    })
+  })
+  
+  client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+  })
+  
