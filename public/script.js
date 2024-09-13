2f0e94b30ac7c478699d234656b9625be6f875af
AllgamerAlex <angel.alexanderbaezflores@gmail.com>
2024-09-12 23:26:04 -0600

    feat: actualización y mejora de la interfaz de usuario

--- public/script.js
+++ public/script.js
@@ -2,11 +2,18 @@ const ws = new WebSocket('ws://localhost:3000');
 const messageList = document.getElementById('message-list');
 const sendButton = document.getElementById('send');
 const messageInput = document.getElementById('message');
+const themeToggle = document.getElementById('theme-toggle');
+const statusBar = document.getElementById('status-bar');
+
+// Toggle de modo oscuro/claro
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');  // Alterna entre 'dark-mode' y 'light-mode'
+});
 
 ws.onopen = () => {
-  document.getElementById('status-bar').textContent = 'Connected to MQTT Broker';
-  document.getElementById('status-bar').classList.remove('disconnected');
-  document.getElementById('status-bar').classList.add('connected');
+  statusBar.textContent = 'Connected to MQTT Broker';
+  statusBar.classList.remove('disconnected');
+  statusBar.classList.add('connected');
 };
 
 ws.onmessage = (event) => {
@@ -16,9 +23,9 @@ ws.onmessage = (event) => {
 };
 
 ws.onclose = () => {
-  document.getElementById('status-bar').textContent = 'Disconnected from MQTT Broker';
-  document.getElementById('status-bar').classList.remove('connected');
-  document.getElementById('status-bar').classList.add('disconnected');
+  statusBar.textContent = 'Disconnected from MQTT Broker';
+  statusBar.classList.remove('connected');
+  statusBar.classList.add('disconnected');
 };
 
 sendButton.addEventListener('click', () => {
